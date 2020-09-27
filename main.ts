b60a30d2b64cbb8766efa6e33054d8db4016cf10
xd-Orange <71978398+xd-Orange@users.noreply.github.com>
2020-09-27 10:26:16 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+// When the user shakes the micro:bit, a frown face appears.
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+// When the micro:bit isn't shaking, a smiley face appears.
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
